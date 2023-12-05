6970b76a53ca7c33a25b9591fdf6eed48a22a43a
adrianpopescupx <adrianpopescupx@gmail.com>
2023-12-05 09:59:41 +0200

    lab01 finished

--- lab01/add_parallel.c
+++ lab01/add_parallel.c
@@ -1,62 +1,78 @@
+#include <math.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-/*
-    schelet pentru exercitiul 5
-*/
 
 int *arr;
 int array_size;
 int num_threads;
 
-void *threadJob(int *arg) {
+struct ThreadArgs {
+    int start;
+    int end;
+};
 
-  return NULL;
+void *threadJob(void *arg) {
+    struct ThreadArgs *args = (struct ThreadArgs *)arg;
+    for (int i = args->start; i < args->end; i++) {
+        arr[i] += 100;
+    }
+    return NULL;
 }
 
 int main(int argc, char *argv[]) {
-  if (argc < 3) {
-    fprintf(stderr, "Specificati dimensiunea array-ului si numarul de thread-uri\n");
-    exit(-1);
-  }
-
-  array_size = atoi(argv[1]);
-  num_threads = atoi(argv[2]);
-
-  arr = (int *)malloc(array_size * sizeof(int));
-  for (int i = 0; i < array_size; i++) {
-    arr[i] = i;
-  }
-
-  for (int i = 0; i < array_size; i++) {
-    printf("%d", arr[i]);
-    if (i != array_size - 1) {
-      printf(" ");
-    } else {
-      printf("\n");
+    if (argc < 3) {
+        fprintf(stderr, "Specificati dimensiunea array-ului si numarul de thread-uri\n");
+        exit(-1);
     }
-  }
 
-  // TODO: aceasta operatie va fi paralelizata cu num_threads fire de executie
-  pthread_t threads[num_threads];
-  int chunkSize = array_size / num_threads; 
-  for (int i = 0; i < num_threads; i++) {
-    int start = i * chunkSize;
-    
-  }
-  for (int i = 0; i < array_size; i++) {
-    arr[i] += 100;
-  }
+    array_size = atoi(argv[1]);
+    num_threads = atoi(argv[2]);
 
-  for (int i = 0; i < array_size; i++) {
-    printf("%d", arr[i]);
-    if (i != array_size - 1) {
-      printf(" ");
-    } else {
-      printf("\n");
+    arr = (int *)malloc(array_size * sizeof(int));
+    for (int i = 0; i < array_size; i++) {
+        arr[i] = i;
     }
-  }
-  free(arr);
-  return 0;
+
+    for (int i = 0; i < array_size; i++) {
+        printf("%d", arr[i]);
+        if (i != array_size - 1) {
+            printf(" ");
+        } else {
+            printf("\n");
+        }
+    }
+
+    struct ThreadArgs *threadArgs = (struct ThreadArgs *)malloc(num_threads * sizeof(struct ThreadArgs));
+
+    pthread_t threads[num_threads];
+    int chunkSize = array_size / num_threads;
+    for (int i = 0; i < num_threads; i++) {
+        int start = i * (array_size / num_threads);
+        int end = fmin((i + 1) * chunkSize, array_size);
+        if (end == array_size - 1)
+          end++;
+        struct ThreadArgs ThreadArgs;
+        threadArgs[i].start = start;
+        threadArgs[i].end = end;
+        pthread_create(&threads[i], NULL, threadJob, &threadArgs[i]);
+    }
+    // for (int i = 0; i < array_size; i++) {
+    //   arr[i] += 100;
+    // }
+    for (int i = 0; i < num_threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    for (int i = 0; i < array_size; i++) {
+        printf("%d", arr[i]);
+        if (i != array_size - 1) {
+            printf(" ");
+        } else {
+            printf("\n");
+        }
+    }
+    // Free dynamically allocated memory
+    free(arr);
+    free(threadArgs);
+    return 0;
 }
